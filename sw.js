171cfff288f41b6929207611d3a91a1833d3195f
polvalls <polvallsrue@gmail.com>
2019-12-08 21:06:57 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3c6561486f3da37fd9ff.js"
+    "url": "webpack-runtime-cf51190f77bf886110d5.js"
   },
   {
     "url": "app-d6ac3b691ab9f23f4bdf.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b10895e4184c9e2b499b57ed1355151b"
+    "revision": "a6cffac6c73b5b5d2e62200da7765ba7"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
