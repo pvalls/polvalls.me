88eafeaa351fb6a22ba6d2648d3c5554d7665db7
pvalls <polvallsrue@gmail.com>
2021-02-01 23:27:40 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2d548d965862bae8ac88.js"
+    "url": "webpack-runtime-296065ef9b688d0b774b.js"
   },
   {
     "url": "app-dd0225b2520b387ed4bc.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d801c2f0e77460da53237f2fde29ad4e"
+    "revision": "8f887053727c762307f9d097075b8b8e"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
